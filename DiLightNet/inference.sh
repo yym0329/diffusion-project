680b4cf9326466c0c4f5e1d26183758c4d392a10
youngmin ryou <yym3055@gmail.com>
2024-11-14 14:21:18 +0000

    init

---
+++ DiLightNet/inference.sh
@@ -0,0 +1,3 @@
+python3 infer_img.py --prov_img examples/provisional_img/lighting/CB2.JPG --prompt "a photo of a toy truck made of plastic" --out_vid ./output/toy_truck.mp4
+python3 infer_img.py --prov_img examples/provisional_img/lighting/obj_14_red_bucket_src_011_tgt_008_CE2.png --prompt "a photo of a red bucket with green apples in it." --out_vid ./output/red_bucket_eval.mp4
+python3 infer_img.py --prov_img examples/provisional_img/lighting/obj_49_rubbery_dinasour_src_007_tgt_002_CE2.png --prompt "A small dragon figure stands upright with wings outstretched, showing a muscular build, scaled texture, and a fierce expression with horns and an elongated snout." --out_vid ./output/rubbery_dinosaur_eval_2.mp4
