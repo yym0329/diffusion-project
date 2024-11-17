1bbc40ede0c4c038f849193c03d5f5a47d238a4a
youngmin ryou <yym3055@gmail.com>
2024-11-17 07:09:20 +0000

    Refactor get_envmap function and improve main processing logic for radiance hints

--- generate_hints.py
+++ generate_hints.py
@@ -8,32 +8,60 @@ import numpy as np
 import cv2
 import simple_parsing
 
+
 # TODO: change to the correct pl points
 def get_envmap(viewpoint_id: str, light_condition_id: str) -> str:
-	if viewpoint_id not in ["NA3", "NE7", "CB5", "CF8", "NA7", "CC7", "CA2", "NE1", "NC3", "CE2"]:
-		raise ValueError(f"Invalid viewpoint_id: {viewpoint_id}")
-	if light_condition_id not in ["001", "002", "003", "004", "005", "006", "007", "008", "009", "010", "011", "012", "013"]:
-		raise ValueError(f"Invalid light_condition_id: {light_condition_id}")
-	
-	return f"envmaps/{viewpoint_id}_{light_condition_id}.hdr"
+    if viewpoint_id not in [
+        "NA3",
+        "NE7",
+        "CB5",
+        "CF8",
+        "NA7",
+        "CC7",
+        "CA2",
+        "NE1",
+        "NC3",
+        "CE2",
+    ]:
+        raise ValueError(f"Invalid viewpoint_id: {viewpoint_id}")
+    if light_condition_id not in [
+        "001",
+        "002",
+        "003",
+        "004",
+        "005",
+        "006",
+        "007",
+        "008",
+        "009",
+        "010",
+        "011",
+        "012",
+        "013",
+    ]:
+        raise ValueError(f"Invalid light_condition_id: {light_condition_id}")
+
+    return f"envmaps/{viewpoint_id}_{light_condition_id}.hdr"
 
 
 @dataclass
 class Args:
-	json_path: str
-	seed: int = 3407  # Seed for the generation
-	fov: Optional[float] = None  # Field of view for the mesh reconstruction, none for auto estimation from the image
-	
-	env_map_path: Optional[str] = None
-	power: float = 1200.  # Power of the point light
-	use_gpu_for_rendering: bool = True  # Use GPU for radiance hints rendering
+    json_path: str
+    seed: int = 3407  # Seed for the generation
+    fov: Optional[float] = (
+        None  # Field of view for the mesh reconstruction, none for auto estimation from the image
+    )
+
+    env_map_path: Optional[str] = None
+    power: float = 1200.0  # Power of the point light
+    use_gpu_for_rendering: bool = True  # Use GPU for radiance hints rendering
 
 
 def main(args: Args):
-	with open(args.json_path, "r") as f:
-		metadata = json.load(f)
-	
-	"""
+    with open(args.json_path, "r") as f:
+        metadata = json.load(f)
+
+    """
 	1. load jsonl (done)
 	2. for each image, load the image and mask
 	3. create a mesh from the image and mask
@@ -49,35 +77,45 @@ def main(args: Args):
 	},
 	...
 	]
+    
+    Then this function saves the rendered radiance hints to the output_dir/image_id/
 	"""
-	from DiLightNet.demo.mesh_recon import mesh_reconstruction
-	from DiLightNet.demo.render_hints import render_hint_images
-	
-		
-	for item in metadata:
-		image_path = item["image_path"]
-		mask_path = item["mask_path"]
-		viewpoint_id = item["viewpoint_id"]
-		lighting_condition_id = item["lighting_condition_id"]
-		
-		# Load the image and mask
-		image = imageio.imread(image_path)
-		mask = imageio.imread(mask_path)
-		
-		# env map
-		env_map = get_envmap(viewpoint_id, lighting_condition_id)
-		# Create a mesh from the image and mask
-		
-		output_folder = os.path.join(args.output_dir, os.path.basename(image_path).split(".")[0])
-		if not os.path.exists(output_folder):
-			os.makedirs(output_folder)
-		
-		print(f"Rendering radiance hints for {image_path} with viewpoint {viewpoint_id} and lighting condition {lighting_condition_id}")
-		# Mesh reconstruction and fov estimation for hints rendering
-		fov = args.fov
-		mesh, fov = mesh_reconstruction(image, mask, False, fov, args.mask_threshold)
-		print(f"Mesh reconstructed with fov: {fov}")
-		render_hint_images(mesh, fov, env_map= env_map , output_folder=output_folder, use_gpu=args.use_gpu_for_rendering)
-		print(f"Radiance hints rendered to {output_folder}")
+    from DiLightNet.demo.mesh_recon import mesh_reconstruction
+    from DiLightNet.demo.render_hints import render_hint_images
 
-		# Need Test
+    for item in metadata:
+        image_path = item["image_path"]
+        mask_path = item["mask_path"]
+        viewpoint_id = item["viewpoint_id"]
+        lighting_condition_id = item["lighting_condition_id"]
+        image_id = item["image_id"]
+
+        # Load the image and mask
+        image = imageio.imread(image_path)
+        mask = imageio.imread(mask_path)
+
+        # env map
+        env_map = get_envmap(viewpoint_id, lighting_condition_id)
+        # Create a mesh from the image and mask
+
+        output_folder = os.path.join(args.output_dir, image_id)
+        if not os.path.exists(output_folder):
+            os.makedirs(output_folder)
+
+        print(
+            f"Rendering radiance hints for {image_path} with viewpoint {viewpoint_id} and lighting condition {lighting_condition_id}"
+        )
+        # Mesh reconstruction and fov estimation for hints rendering
+        fov = args.fov
+        mesh, fov = mesh_reconstruction(image, mask, False, fov, args.mask_threshold)
+        print(f"Mesh reconstructed with fov: {fov}")
+        render_hint_images(
+            mesh,
+            fov,
+            env_map=env_map,
+            output_folder=output_folder,
+            use_gpu=args.use_gpu_for_rendering,
+        )
+        print(f"Radiance hints rendered to {output_folder}")
+
+        # Need Test
